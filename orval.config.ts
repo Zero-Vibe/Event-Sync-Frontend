969c3c01aaf90f61f69647c5364bf9e4c4ae4005
Jerry Tanjaka <tanjakajerryjacques@gmail.com>
2026-05-09 00:48:39 +0300

    feat : add  config overall for service generating via OAS

---
+++ orval.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'orval';
+
+export default defineConfig({
+  eventsync: {
+    input: './OAS.yaml',
+    output: {
+      mode: 'tags-split',
+      target: './src/api/generated',
+      client: 'fetch',           
+      clean: true,
+      override: {
+        mutator: {
+          path: './src/api/client.ts',
+          name: 'customFetch',
+        },
+      },
+    },
+  },
+});
